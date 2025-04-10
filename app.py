48bab5d01e75b97cf12419ba779491273a5fd71e
Yugesh <77796731+yugesharma@users.noreply.github.com>
2025-04-10 13:23:19 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.vectorstores import Pinecone
 from sentence_transformers import SentenceTransformer
 from langchain.chains.question_answering import load_qa_chain
-from pinecone Pinecone
+# from pinecone Pinecone
 import os
 
 from huggingface_hub import hf_hub_download
