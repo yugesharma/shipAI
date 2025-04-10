e917b337dfec0279f30fb5fe8cd289d7a067a052
Yugesh <77796731+yugesharma@users.noreply.github.com>
2025-04-10 13:25:15 -0400

    Update app.py

--- app.py
+++ app.py
@@ -28,32 +28,5 @@ def main():
 
     st.write("Website is under maintenance due to Pinecone documentation changes. Thank you for your patience")
 
-    '''
-    #pinecone for vector database
-     pc = Pinecone(api_key=pinecone_api_key)
-
-    embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-mpnet-base-v2')
-    index_name="shipdata"
-
-    #docsearch=Pinecone.from_texts([t.page_content for t in docs], embeddings, index_name=index_name)
-    docsearch = Pinecone.from_existing_index(index_name, embeddings)
-
-    #processing user query
-    try:
-        query = st.text_input("Ask a question here:")
-        if query:
-            docs=docsearch.similarity_search(query)
-
-            llm=ChatOpenAI()
-            chain = load_qa_chain(llm, chain_type="stuff")
-            with get_openai_callback() as cb:
-                response = chain.run(input_documents=docs, question=query, max_tokens=3500)          
-                st.write(response)
-
-
-    except openai.error.InvalidRequestError as e:
-        st.error("An error occurred while processing your request. Please try again later.")
-        st.write(f"Error details: {str(e)}")
-'''
 if __name__ == '__main__':
      main()
